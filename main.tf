892daea959443c7f8a810cebba5cadbf276fb8b8
talomina <76668907+talomina@users.noreply.github.com>
2024-02-17 19:40:21 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,38 @@
 provider "google" {
 }
-# [vpc_auto_create の開始]
 resource "google_compute_network" "vpc_network" {
-project = var.project_id
-name = "my-auto-mode-network"
-auto_create_subnetworks = true
-mtu = 1460
+  name                    = "my-custom-mode-network"
+  auto_create_subnetworks = false
+  mtu                     = 1460
 }
-# [vpc_firewall_create の開始]
-resource "google_compute_firewall" "rules" {
-project = var.project_id
-name = "my-firewall-rule"
-network = "vpc_network"
-allow {
-protocol = "tcp"
-ports = ["80", "8080"]
+
+resource "google_compute_subnetwork" "default" {
+  name          = "my-custom-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-west1"
+  network       = google_compute_network.vpc_network.id
 }
+# Create a single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = "flask-vm"
+  machine_type = "f1-micro"
+  zone         = "us-west1-a"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  # Install Flask
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.id
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
 }
